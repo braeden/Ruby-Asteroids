59ab01c3b9dc6cd4ae91908d8514104ffa92b426
Braeden123 <braedensmith2@gmail.com>
2015-06-13 17:17:22 -0400

    commit

--- asteroids.rb
+++ asteroids.rb
@@ -16,11 +16,24 @@ Ray.game "Asteroids", :size => [800, 600] do
     @bullets = []
     @bull_vel_x = 0.0
     @bull_vel_y = 0.0
-    @asteroids = 5.times.map do
+    @lives = 3
+    @score = 0
+    @asteroids = 3.times.map do
       a = Ray::Polygon.rectangle([0, 0, 50, 50], Ray::Color.white)
-      a.pos = [rand(0..30), rand(0..600)]
-      a.filled   = false
+      a.pos = [rand(0..200), rand(0..600)]
+      a.filled = false
       a.outlined = true
+      a.outline = Ray::Color.white
+      a
+    end
+    @asteroids += 3.times.map do
+      a = Ray::Polygon.rectangle([0, 0, 50, 50], Ray::Color.white)
+      a.pos = [rand(600..800), rand(0..600)]
+      a.filled = false
+      a.outlined = true
+      a.outline = Ray::Color.white
+      #[rand(-3...3), rand(-3...3)]
+
       a
     end
     #on :key_press, key(:q){ exit! }
@@ -28,8 +41,8 @@ Ray.game "Asteroids", :size => [800, 600] do
     always do
 
       if holding? key(:up)
-        @vel_x += (@ship.angle / (180 / Math::PI))
-        @vel_y -= (@ship.angle / (180 / Math::PI))
+        @vel_x += Math::sin(@ship.angle / (180 / Math::PI)) * 0.5
+        @vel_y -= Math::cos(@ship.angle / (180 / Math::PI)) * 0.5
       end
       if holding? key(:left)
         @ship.angle -= 4.5
@@ -62,6 +75,7 @@ Ray.game "Asteroids", :size => [800, 600] do
           b
         end
       end
+
       @bullets.each do |b|
         if b.pos.x > 800
           @bullets.delete(b)
@@ -75,14 +89,26 @@ Ray.game "Asteroids", :size => [800, 600] do
         b.pos += [@bull_vel_x, @bull_vel_y]
         b
       end
+      @lives -= 1 if @asteroids.any? { |a|
+        [a.pos.x, a.pos.y, 50, 50].to_rect.collide?([@ship.pos.x, @ship.pos.y, 16,20])
+      }
+      #; @ship.pos = [400,290]; @vel_x = 0.0; @vel_y = 0.0
     end
     render do |win|
-      win.draw @ship
-      @bullets.each do |b|
-        win.draw b
-      end
-      @asteroids.each do |a|
-        win.draw a
+    #  if @game_over
+        #win.draw text("YOU LOST", :at => [180,180], :size => 60)
+      if @lives <= 0
+        win.draw text("YOU LOST", :at => [180,180], :size => 60)
+      else
+        win.draw @ship
+        win.draw text("Lives:" + @lives.to_s, :at => [0,0], :size => 20)
+        win.draw text("Score:" + @score.to_s, :at => [100,0], :size => 20)
+        @bullets.each do |b|
+          win.draw b
+        end
+        @asteroids.each do |a|
+          win.draw a
+        end
       end
     end
   end
